c8312bce4e0412b674e411f1b112d585d343311e
vikas-11-dixena <vikasdixena79@gmail.com>
2024-08-21 12:16:38 +0530

    added todo components

---
+++ src/components/Todo/Todo.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+function Todo({todoData}) {
+  return (
+    <div>
+      <input type='checkbox' />
+      {todoData}
+      <button>Edit</button>
+      <button>Delete</button>
+    </div>
+  )
+}
+
+export default Todo
